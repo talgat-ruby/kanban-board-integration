8686496d13ecd9f08d7b5eae5268e523d89aa27f
Talgat <talgat.s@protonmail.com>
2023-04-13 02:05:46 +0600

    add tables migrations

---
+++ hasura/migrations/default/1681328683990_create_tables_boards_columns_tasks_subtasks/down.sql
@@ -0,0 +1,31 @@
+
+alter table "public"."columns" drop constraint "columns_board_id_fkey";
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."columns" add column "board_id" uuid
+--  null;
+
+alter table "public"."tasks" drop constraint "tasks_column_id_fkey";
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."tasks" add column "column_id" uuid
+--  null;
+
+alter table "public"."subtasks" drop constraint "subtasks_task_id_fkey";
+
+alter table "public"."subtasks" rename column "task_id" to "task_d";
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."subtasks" add column "task_d" uuid
+--  null;
+
+DROP TABLE "public"."boards";
+
+DROP TABLE "public"."columns";
+
+DROP TABLE "public"."tasks";
+
+DROP TABLE "public"."subtasks";
---
+++ hasura/migrations/default/1681328683990_create_tables_boards_columns_tasks_subtasks/up.sql
@@ -0,0 +1,105 @@
+
+CREATE TABLE "public"."subtasks" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "title" text NOT NULL, "isCompleted" boolean NOT NULL DEFAULT false, PRIMARY KEY ("id") );COMMENT ON TABLE "public"."subtasks" IS E'subtasks of tasks which are basically checklists';
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_subtasks_updated_at"
+BEFORE UPDATE ON "public"."subtasks"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_subtasks_updated_at" ON "public"."subtasks"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE "public"."tasks" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "title" text NOT NULL, "description" text NOT NULL, PRIMARY KEY ("id") );COMMENT ON TABLE "public"."tasks" IS E'column tasks';
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_tasks_updated_at"
+BEFORE UPDATE ON "public"."tasks"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_tasks_updated_at" ON "public"."tasks"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE "public"."columns" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "name" text NOT NULL, PRIMARY KEY ("id") );COMMENT ON TABLE "public"."columns" IS E'columns of a board';
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_columns_updated_at"
+BEFORE UPDATE ON "public"."columns"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_columns_updated_at" ON "public"."columns"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE "public"."boards" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), "name" text NOT NULL, PRIMARY KEY ("id") );COMMENT ON TABLE "public"."boards" IS E'kanban boards';
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_boards_updated_at"
+BEFORE UPDATE ON "public"."boards"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_boards_updated_at" ON "public"."boards"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+alter table "public"."subtasks" add column "task_d" uuid
+ null;
+
+alter table "public"."subtasks" rename column "task_d" to "task_id";
+
+alter table "public"."subtasks"
+  add constraint "subtasks_task_id_fkey"
+  foreign key ("task_id")
+  references "public"."tasks"
+  ("id") on update cascade on delete cascade;
+
+alter table "public"."tasks" add column "column_id" uuid
+ null;
+
+alter table "public"."tasks"
+  add constraint "tasks_column_id_fkey"
+  foreign key ("column_id")
+  references "public"."columns"
+  ("id") on update cascade on delete cascade;
+
+alter table "public"."columns" add column "board_id" uuid
+ null;
+
+alter table "public"."columns"
+  add constraint "columns_board_id_fkey"
+  foreign key ("board_id")
+  references "public"."boards"
+  ("id") on update cascade on delete cascade;
