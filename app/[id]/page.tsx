ad5f72bdcfb9c47b869bc74a298a831c002599ea
Talgat <talgat.s@protonmail.com>
2023-04-19 15:34:21 +0600

    Squashed 'ui/' changes from 7b534fd..3fbeb3b
    3fbeb3b Add mutations of boards
    
    git-subtree-dir: ui
    git-subtree-split: 3fbeb3bff915eb69f4fe2c65b822d24637b5d044

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -10,7 +10,7 @@ interface IProps {
 export default function Board({ params }: IProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Sidebar />
+      <Sidebar boardId={params.id} />
       <BoardViewer id={params.id} />
     </main>
   );
--- components/Aside/Aside.tsx
+++ components/Aside/Aside.tsx
@@ -1,16 +1,91 @@
 "use client";
 
-import { useState } from "react";
+import {
+  ChangeEvent,
+  FormEvent,
+  MouseEvent,
+  useCallback,
+  useState,
+} from "react";
 import clsx from "clsx";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { IBoard } from "@/types/boards";
 
 interface IProps {
+  boardId?: string;
   boards: IBoard[];
 }
 
-function Aside({ boards }: IProps) {
+function Aside({ boardId, boards }: IProps) {
+  const router = useRouter();
   const [isShowing, setIsShowing] = useState(true);
+  const [columns, setColumns] = useState<string[]>([]);
+
+  const handleSubmit = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const { currentTarget } = event;
+
+      const formData = new FormData(currentTarget);
+
+      const boardName = formData.get("board-name");
+
+      try {
+        const req = new Request("http://localhost:3000/api/boards", {
+          method: "POST",
+          body: JSON.stringify({
+            name: boardName,
+            columnsData: columns.map((column) => ({ name: column })),
+          }),
+        });
+        const res = await fetch(req);
+        const json: { id: string } = await res.json();
+
+        router.push(`/${json.id}`);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    [columns, router]
+  );
+
+  const handleDelete = useCallback(
+    async (event: MouseEvent<HTMLButtonElement>) => {
+      const { dataset } = event.currentTarget;
+      const id = dataset.id || "";
+
+      try {
+        const res = await fetch(`http://localhost:3000/api/boards/${id}`, {
+          method: "DELETE",
+        });
+        const json: { id: string; name: string } = await res.json();
+
+        if (boardId === id) {
+          router.push("/");
+        } else {
+          router.refresh();
+        }
+
+        alert(`Board ${json.name} was deleted`);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    [router]
+  );
+
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const { currentTarget } = event;
+    const { value, dataset } = currentTarget;
+    const ind = Number(dataset.ind);
+
+    setColumns((columns) => [
+      ...columns.slice(0, ind),
+      value,
+      ...columns.slice(ind + 1),
+    ]);
+  }, []);
 
   return (
     <aside>
@@ -18,9 +93,48 @@ function Aside({ boards }: IProps) {
         {boards.map((board) => (
           <li key={board.id}>
             <Link href={`/${board.id}`}>{board.name}</Link>
+            <button>Update</button>
+            <button data-id={board.id} onClick={handleDelete}>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="board-name">Name</label>
+        <input type="text" id="board-name" name="board-name" />
+
+        <label htmlFor="">Column</label>
+        <ul>
+          {columns.map((column, ind) => (
+            <li key={ind}>
+              <input
+                type="text"
+                data-ind={ind}
+                value={column}
+                onChange={handleChange}
+              />
+              <button
+                type="button"
+                onClick={() =>
+                  setColumns((prevColumns) =>
+                    prevColumns.filter((_, prevInd) => prevInd !== ind)
+                  )
+                }
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button
+          type="button"
+          onClick={() => setColumns((columns) => [...columns, ""])}
+        >
+          Add new column
+        </button>
+        <button type="submit">Create new board</button>
+      </form>
       <button onClick={() => setIsShowing((prevIsShowing) => !prevIsShowing)}>
         {isShowing ? "Hide" : "Show"}
       </button>
